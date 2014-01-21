221364e7e50b90e865b2f0b521f95b6fb459c9f9
alun <alun@alun-no-MacBook-Air.local>
2014-01-21 20:55:07 +0900

    added bootstrap gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 RailsBasic1::Application.routes.draw do
+  resources :posts
+
   get "home/index"
   root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
